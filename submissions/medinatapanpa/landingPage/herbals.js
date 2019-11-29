ecc76c7e2b8b9f4cfc9792f670869eb55b1d2374
abolayo <medinatapanpa@gmail.com>
2019-11-29 22:26:09 +0100

    I liked my sign up page

--- submissions/medinatapanpa/landingPage/herbals.js
+++
@@ -1,7 +0,0 @@
-const container = document.querySelector('#container');
-
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'hi i am red!';
-div.style.color = 'red';   
-container.appendChild(content);
