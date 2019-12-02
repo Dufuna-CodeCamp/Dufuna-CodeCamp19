5c448b88721ba0b5a23ef8d4848f4d752a7ef3c9
jemikalajah123 <okeoghene.philip5@gmail.com>
2019-12-02 13:00:56 +0100

    updated css file for my homepage, code syntax and folder structures for product images

--- submissions/Jemikalajah-Philip/javascript/homepage.js
+++ submissions/Jemikalajah-Philip/javascript/homepage.js
@@ -1,8 +1,9 @@
 let imgDiv = document.getElementsByClassName("product--image");
 let prodTitle = document.getElementsByClassName("product--title");
 let prodPrice = document.getElementsByClassName("product--price");
-document.getElementById("prod").addEventListener("click", displayMenProduct);
-var productArray = [
+document.getElementById("men-category").addEventListener("click", displayMenPolo);
+document.getElementById("women-category").addEventListener("click", displayWomenDress);
+var productArrayMen = [
      {image: "../html/product-images/img19.jpg", title: "Round Neck Polo", price: "$400"}, 
      {image: "../html/product-images/img21.jpg",  title: "V-neck Polo", price: "$300"},
      {image: "../html/product-images/img20.jpg", title: "turtle neck Polo", price: "$200"},
@@ -13,10 +14,28 @@ var productArray = [
      {image: "../html/product-images/img9.jpeg", title: "White Polo", price: "$800"},
      {image: "../html/product-images/img8.jpeg", title: "chinko Polo", price: "$460"}
 ];
-function displayMenProduct(){
-    for (i = 0; i < productArray.length; i++){
-    imgDiv[i].setAttribute("src", productArray[i].image);
-    prodTitle[i].innerHTML = productArray[i].title;
-    prodPrice[i].innerHTML = productArray[i].price; 
+var productArrayWomen = [
+    {image: "../html/product-images/img27.jpg", title: "stearl dress ", price: "$320"}, 
+    {image: "../html/product-images/img26.jpg",  title: "truu Dress", price: "$1300"},
+    {image: "../html/product-images/img25.jpeg", title: "blouse & skirt", price: "$2200"},
+    {image: "../html/product-images/img28.jpg", title: " flower dress", price: "$6200"},
+    {image: "../html/product-images/img29.jpg", title: "Dura ", price: "$4000"},
+    {image: "../html/product-images/img30.jpg", title: " blue flower dress", price: "$5200"},
+    {image: "../html/product-images/img31.jpg", title: "pearl dress ", price: "$900"},
+    {image: "../html/product-images/img32.jpg", title: "bluestar ", price: "$300"},
+    {image: "../html/product-images/img33.jpg", title: "Pinkstar", price: "$860"}
+];
+function displayMenPolo(){
+    for (i = 0; i < productArrayMen.length; i++){
+    imgDiv[i].setAttribute("src", productArrayMen[i].image);
+    prodTitle[i].innerHTML = productArrayMen[i].title;
+    prodPrice[i].innerHTML = productArrayMen[i].price; 
+    }    
+}
+function displayWomenDress(){
+    for (i = 0; i < productArrayWomen.length; i++){
+    imgDiv[i].setAttribute("src", productArrayWomen[i].image);
+    prodTitle[i].innerHTML = productArrayWomen[i].title;
+    prodPrice[i].innerHTML = productArrayWomen[i].price; 
     }    
 }
