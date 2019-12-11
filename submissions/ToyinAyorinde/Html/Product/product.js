afd0150dca9040042cf90b795e55f5d38032742e
Temitoyin Ayorinde <tjayorinde@gmail.com>
2019-12-11 12:08:42 +0100

    changes made to pull request review

--- submissions/ToyinAyorinde/Html/Product/product.js
+++ submissions/ToyinAyorinde/Html/Product/product.js
@@ -21,16 +21,12 @@ let products = [
 ]
 
 let imageDiv = document.getElementsByClassName('prdt-img');
-console.log(imageDiv);
 let productName = document.getElementsByClassName('product-name');
-console.log(productName);
 let productPrice = document.getElementsByClassName('product-price');
-console.log(productPrice);
 let button  = document.getElementById("btn");
-console.log(button);
-button.addEventListener('click', displayProduct);
+button.addEventListener('click', displayProducts);
 
-function displayProduct() {
+function displayProducts() {
    for(let i = 0; i < products.length; i++) {
        imageDiv[i].src = products[i].imageUrl;
        productName[i].innerHTML = products[i].name;
