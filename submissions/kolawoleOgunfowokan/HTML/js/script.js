dad93e5b29c6303d9986138d3fe5782c887f12cc
kolawolejohn <kolawole.oajohn@gmail.com>
2019-12-17 11:53:06 +0100

    I was able to target the button to display the product

--- submissions/kolawoleOgunfowokan/HTML/js/script.js
+++ submissions/kolawoleOgunfowokan/HTML/js/script.js
@@ -1,74 +1,91 @@
 
   let productArray = 
-[
-     { 
-       image: "../images/eutetic-freezer.jpg",
-       title: "Freezer",
-       price: "N195000"
-     },
-
-     {
-       image: "../images/huaweiMateBookXPro.jpg",
-       title: "Huwaei Laptop",
-       price: "N200000"
-     },
-
-     {
-       image: "../images/java-prog.png",
-       title: "Java Book",
-       price: "N12000"
-     },
-
-     {
-       image: "../images/microsoftSurface.jpg",
-       title: "Microsoft Surface",
-       price: "N320000"
-     },
-
-     {
-       image: "../images/introprog.jpg",
-       title: "Introduction to Programming",
-       price: "N10000"
-     },
-
-     {
-       image: "../images/Macbook.jpg",
-       title: "Macbook pro",
-       price: "N490000"
-     },
-
-     {
-       image: "../images/AppleDesktop.png",
-       title: "Apple Desktop",
-       price: "N350000"
-     },
-];
-
-let displayContent = document.getElementById("product-box");
+  [
+      { 
+        image: "../images/HP250.jpg",
+        title: "HP Laptop",
+        price: "N130000"
+      },
   
-displayContent.addEventListener("click", function() {
-  displayProducts();
-});
-
-function displayProducts() {
-  productArray.forEach(element => {
-      let productArray = `
-      <div class="product-box">
-      <div class="product-content">
-          <div class="product-image">
-              <img src="${element.image}"  class="images" id="image"/>
-          </div>
-         <div>
-             <h3 class="product-title">
-               ${element.title}
-             </h3>
-         </div> 
-         <div class="product-price">
-              ${element.price}
-         </div>
-      </div>
-  </div>
-      `;
-      displayContent.innerHTML += productArray;
+      { 
+         image: "../images/eutetic-freezer.jpg",
+         title: "Freezer",
+         price: "N195000"
+       },
+  
+       {
+         image: "../images/huaweiMateBookXPro.jpg",
+         title: "Huwaei Laptop",
+         price: "N200000"
+       },
+  
+       {
+         image: "../images/java-prog.png",
+         title: "Java Book",
+         price: "N12000"
+       },
+  
+       {
+         image: "../images/microsoftSurface.jpg",
+         title: "Microsoft Surface",
+         price: "N320000"
+       },
+  
+       {
+         image: "../images/introprog.jpg",
+         title: "Introduction to Programming",
+         price: "N10000"
+       },
+  
+       {
+         image: "../images/Macbook.jpg",
+         title: "Macbook pro",
+         price: "N490000"
+       },
+  
+       {
+         image: "../images/AppleDesktop.png",
+         title: "Apple Desktop",
+         price: "N350000"
+       },
+  ];
+  
+  let button = document.getElementById("btn");
+  let displayContent = false;
+  let products = document.getElementById("product-box");
+    
+  button.addEventListener("click", function() {
+    displayProducts();
   });
-}
+  
+  function displayProducts() {
+    displayContent = !displayContent;
+    if(displayContent == true){
+        productArray.forEach(element => {
+        let productArray = `
+           <div class="product-box" id="product-box">
+              <div class="product-content">
+                <div class="product-image">
+                  <img src="${element.image}"  class="images">
+                </div>
+                <div>
+                  <h3 class="product-title">
+                     ${element.title}
+                  </h3>
+                </div> 
+                <div class="product-price">
+                     ${element.price}
+                </div>
+            </div>
+          </div>`;
+        products.innerHTML += productArray;
+    });
+  }
+  
+    else {
+       products = document.getElementById("btn").disabled;
+    }
+   
+  }
+  
+  
