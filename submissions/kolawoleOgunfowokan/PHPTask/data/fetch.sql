91cd7a65f4d49257229e76d6d86328b3e061c35d
kolawolejohn <kolawole.oajohn@gmail.com>
2020-01-16 10:24:57 +0100

    This is my PHP Task

---
+++ submissions/kolawoleOgunfowokan/PHPTask/data/fetch.sql
@@ -0,0 +1,3 @@
+USE myoja;
+
+SELECT * FROM brands;
---
+++ submissions/kolawoleOgunfowokan/PHPTask/php/create.php
@@ -0,0 +1,61 @@
+<?php
+if (isset($_POST['submit'])) {
+    require "config.php";
+    require "helper.php";
+
+    try {
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $newProduct = array(
+            "itemname" => $_POST['itemname'],
+            "quantity" => $_POST['quantity'],
+            "brand_id" => $_POST['brand'] + 1
+        );
+
+        /**
+         * This is equivalent to
+         * INSERT INTO products (itemname, quantity) values (:itemname, :quantity)
+         */
+        $sql = sprintf(
+            "INSERT INTO %s (%s) values (%s)",
+            "products",
+            implode(", ", array_keys($newProduct)),
+            ":" . implode(", :", array_keys($newProduct))
+        );
+
+        $statement = $connection->prepare($sql);
+        $statement->execute($newProduct);
+    } catch (PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+?>
+
+<?php if (isset($_POST['submit']) && $statement) { ?>
+    > <?php echo $_POST['itemname']; ?> successfully added.
+<?php } ?>
+
+
+<h2>Add A Product </h2>
+
+<form method="post">
+    <div class="form-container">
+        <label for="itemname">Name of Product</label>
+        <input type="text" name="itemname" id="itemname">
+        <?php session_start(); ?>
+
+        <select name="brand">
+            <?php for ($i = 0; $i < count($_SESSION['brands']); $i++) { ?>
+                <option value= <?php echo $i; ?>> <?php echo $_SESSION['brands'][$i] ?></option>
+            <?php } ?>
+
+        </select>
+
+        <label for="quantity">Quantity</label>
+        <input type="text" name="quantity" id="quantity">
+
+        <input type="submit" name="submit" value="Submit">
+    </div>
+</form>
+<?php echo '<link  href="../HTML/css/style.css" rel="stylesheet"/>'; ?>
+<?php require "footer.php"; ?>
