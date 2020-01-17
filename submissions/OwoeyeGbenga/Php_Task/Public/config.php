340dc3269b7765446aa843e1b678a34f3cdca85c
Gbenga <gbengulo005@gmail.com>
2020-01-17 23:56:53 +0100

    I created a database for products and a page where products can be uploaded to te db

---
+++ submissions/OwoeyeGbenga/Php_Task/Public/config.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dufunahelp";
+$dsn = "mysql:host=$host;dbname=$dbname";
+$options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+);
