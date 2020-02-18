f9f3408d8ac0014f7a0499dc859922a5e99839cc
Emmanuel <eudoh940@gmail.com>
2020-02-18 12:39:48 -0800

    created phpIntro

---
+++ submissions/EmmanuelUdoh/phpIntro/php/overview.php
@@ -0,0 +1,40 @@
+<?php
+require "helper.php";
+require "config.php";
+
+try {
+    $connection = new PDO("mysql:host=$host", $username, $password, $options);
+    $stmt = $connection->query("SELECT * FROM kimaj.products");
+    $products = array();
+    while ($item = $stmt->fetch()) {
+        array_push($products, $item);
+    }
+} catch (PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+}
+?>
+
+<?php require "header.php"; ?>
+
+<h2>All Products</h2>
+<table>
+    <thead>
+        <tr>
+            <th>Product id</th>
+            <th>Item Name</th>
+            <th>Brand Id</th>
+            <th>Date</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($products as $row) : ?>
+            <tr>
+                <td><?php echo escape($row["id"]); ?></td>
+                <td><?php echo escape($row["itemname"]); ?></td>
+                <td><?php echo escape($row["brand_id"]); ?></td>
+                <td><?php echo escape($row["date"]); ?> </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<a href="create.php">Back to Create</a>
---
+++ submissions/EmmanuelUdoh/phpIntro/sql/init.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE kimaj;
+ 
+ USE kimaj;
+
+ CREATE TABLE brands (
+  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT, PRIMARY KEY,
+  brand_name VARCHAR(30) NOT NULL,
+  brand_description VARCHAR(100) NOT NULL,
+);
+
+CREATE TABLE products (
+  id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  brand_id INT(11) UNSIGNED NOT NULL,
+  itemname VARCHAR(30) NOT NULL,
+  quantity INT(3),
+  PRIMARY KEY (id),
+  FOREIGN KEY (brand_id) REFERENCES brands (id) ON DELETE CASCADE,
+  date TIMESTAMP,
+
+INSERT INTO brands(id, brand_name, brand_description)
+VALUES
+  ("Sharp", "An interesting world"),
+  ("Jump", "Life is Good"),
+  ("Philip", "The stars"),
+  ("Binatone", "Your best power manager"),
+  ("Bedmate",  "Sleep well");
+
+
+);
