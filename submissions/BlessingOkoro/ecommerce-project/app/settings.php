cab42ec42eb6e246de87f2de55b26efc1df2c49a
blessing <bnokoro@gmail.com>
2020-04-28 21:46:57 +0100

    set up for Api with two endpoints

---
+++ submissions/BlessingOkoro/ecommerce-project/app/settings.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+use DI\ContainerBuilder;
+use Monolog\Logger;
+
+return function (ContainerBuilder $containerBuilder) {
+    // Global Settings Object
+    $containerBuilder->addDefinitions([
+        'settings' => [
+            'displayErrorDetails' => true, // Should be set to false in production
+            'logger' => [
+                'name' => 'slim-app',
+                'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+                'level' => Logger::DEBUG,
+            ],
+
+            'db' => [
+                "dbhost" => "localhost",
+                "dbname" => "frozen_fresh",
+                "dbuser" => "root",
+                "dbpass" => ""
+            ]
+        ],
+    ]);
+};
